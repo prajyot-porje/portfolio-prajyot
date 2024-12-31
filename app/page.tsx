e9a83afea932d695e5cd35db855f3e4d153c55af
prajyot-porje <prajyotyt18@gmail.com>
2024-12-31 23:08:44 +0530

    hero section [#4]

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+'use client'
 import About from "@/components/about";
 import Hero from "@/components/Hero";
 
@@ -5,6 +6,7 @@ import Hero from "@/components/Hero";
 export default function Home() {
   return (
     <div className="overflow-hidden h-screen w-screen scroll overflow-y-scroll no-scrollbar">
+
       <Hero/>
       <About/>
     </div>
--- components/about.tsx
+++ components/about.tsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import Image from "next/image";
+import React from "react";
 
 const About = () => {
   return (
-    <div className='h-full w-screen bg-black '>about</div>
-  )
-}
+    <div className="h-screen w-screen try-background flex items-start justify-center">
+      <div className="h-[50rem] w-full dark:bg-grid-black/[0.2] bg-grid-white/[0.08] relative flex items-center justify-center">
+      <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-black [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
 
-export default About
+      </div>
+    </div>
+  );
+};
+
+export default About;
