41e677961729ceee34ed1ceb74520747293f4288
prajyot-porje <prajyotyt18@gmail.com>
2025-02-16 19:58:37 +0530

    Remove unused files and components; clean up project structure

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono, Work_Sans } from "next/font/google";
+import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -12,12 +11,6 @@ const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
 });
-
-const workSans = Work_Sans({
-  variable: "--font-work-sans",
-  subsets: ["latin"],
-  weight:'400',
-})
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -34,12 +27,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <main className={workSans.className} >
-
         {children}
-        
-        </main>
-          
       </body>
     </html>
   );
--- components/ContactSection.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const ContactSection = () => {
-  return (
-    <div className='h-screen w-screen bg-[#38bc1d] dark:bg-dark'>ContactSection</div>
-  )
-}
-
-export default ContactSection
